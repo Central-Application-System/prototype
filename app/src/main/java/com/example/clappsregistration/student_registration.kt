c80b376125bfcc350cc18a7ac6a9a7e7a626488d
You <blessingmalaza.1@gmail.com>
2024-01-19 20:18:40 +0200

    prototype

---
+++ app/src/main/java/com/example/clappsregistration/student_registration.kt
@@ -0,0 +1,11 @@
+package com.example.clappsregistration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class student_registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_registration)
+    }
+}
